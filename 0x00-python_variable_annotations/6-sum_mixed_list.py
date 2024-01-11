41d7ea0d8e575ef77dcf66a0de3f41e59668385e
valariembithe <valmbithe691@gmail.com>
2024-01-11 12:47:01 +0300

    Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" A module that performs function sum_mixed_list"""
+from typing import List
+
+
+def sum_mixed_list(mxd_list: List[int, float]) -> float:
+    """ returns sum as float"""
+    return float(sum(mxd_list))
