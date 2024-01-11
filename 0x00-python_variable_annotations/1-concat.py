83494ff977ba344b6b6b2fb988de4e86bd21537f
valariembithe <valmbithe691@gmail.com>
2024-01-11 11:52:52 +0300

    Tasks

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A module that performs a function concat """
+
+
+def concat(str1: string, str2: string) -> string:
+    """ Returns a concatenated string"""
+    return str1 + str2
