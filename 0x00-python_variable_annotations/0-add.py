4c00ebd18c559534313149e82fab9165657d63f4
valariembithe <valmbithe691@gmail.com>
2024-01-11 11:48:56 +0300

    Tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/user/bin/python3
+""" A module that writes a type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """ Returns sum of float a and float b """
+    return a + b
