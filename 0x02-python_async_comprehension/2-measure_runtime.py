51bc92ed57201810e8955878c5e5895cce0ec815
valariembithe <valmbithe691@gmail.com>
2024-01-16 12:59:21 +0300

    Task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,4 +12,4 @@ async def measure_runtime() -> float:
     """Returns total execution time"""
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    return time.time - start_time
+    return time.time() - start_time
