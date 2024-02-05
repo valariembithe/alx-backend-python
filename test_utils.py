3c1218679f2cae3e18a4789654392aab9c850560
valariembithe <valmbithe691@gmail.com>
2024-02-05 15:23:17 +0300

    Tasks

--- test_utils.py
+++ test_utils.py
@@ -21,14 +21,25 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
 
     def test_access_nested_map(
-        self,
-        nested_map: Dict,
-        path: Tuple[str],
-        expected: Union[Dict, int],
-    ) -> None:
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
         @parameterized.expand([
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
+
+    def test_access_nested_map_execution(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
